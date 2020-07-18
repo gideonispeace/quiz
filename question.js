dec19081d391bacb17e218e8d6543454a012127d
gideonispeace <39224847+gideonispeace@users.noreply.github.com>
2020-07-18 18:54:15 -0500

    Quiz codes

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "What was the name of the first man created by God ?",
+	"option1": "Jesus",
+	"option2": "God",
+	"option3": "Adam",
+	"option4": "Non above",
+	"answer": "2"
+}, {
+	"question": "What is the name of Christ's father in human flesh ?",
+	"option1": "David",
+	"option2": "Abraham",
+	"option3": "Joseph",
+	"option4": "Betuel",
+	"answer": "3"
+}, {
+	"question": "He was the richest King of Isreal ?",
+	"option1": "Solomon",
+	"option2": "Eve",
+	"option3": "Jesus",
+	"option4": "Emmanuel",
+	"answer": "1"
+}, {
+	"question": "Abraham is ?",
+	"option1": "The father of Isreal",
+	"option2": "America",
+	"option3": "Ghana",
+	"option4": "All of the above",
+	"answer": "1"
+}, {
+	"question": "The most used bible language is ?",
+	"option1": "King James version",
+	"option2": "NIV",
+	"option3": "NAB",
+	"option4": "NRSV",
+	"answer": "1"
+}, {
+	"question": "Is gambling a sin",
+	"option1": "Maybe",
+	"option2": "Yes",
+	"option3": "It depends",
+	"option4": "When its convenient",
+	"answer": "1"
+}, {
+	"question": "What is the importance of Christian baptism ?",
+	"option1": "baptizing, teaching all to obey his words",
+	"option2": "Give birth to 2",
+	"option3": "Increase church",
+	"option4": "None",
+	"answer": "3"
+}, {
+	"question": "What does the Bible say about divorce and remarriage ?",
+	"option1": "Sometimes good",
+	"option2": "It is very good",
+	"option3": "I love it",
+	"option4": "I hate divorce",
+	"answer": "4"
+}, {
+	"question": "What does the Bible say about Christian tithing? Should a Christian tithe ?",
+	"option1": "100%",
+	"option2": "2%",
+	"option3": "10%",
+	"option4": "25%",
+	"answer": "3"
+}, {
+	"question": "Who was Cain’s wife??",
+	"option1": "Angel",
+	"option2": "Mabel",
+	"option3": "Cain’s wife was his sister",
+	"option4": "None above",
+	"answer": "3"
+}];
